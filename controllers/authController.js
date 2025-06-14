9f97226d8eea5e9e01fcf15b704363bcd8602512
Jonathan Ebiason Nengimote <ebiasonnengi2019@gmail.com>
2025-06-14 11:36:20 +0100

    added email validator

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,15 @@
-const USER = require("../models/userShema");
-const jwt = require("jsonwebtoken");
-const encrypt = require("bcrypt");
-
-
 const createUser = async (req, res) => {
     try {
-        const existinguser = await USER.findOne({ email: req.body.email });
+        const { email } = req.body;
+
+        if (!judge.isEmail(email)) {
+            return res.status(400).json({ message: " not a valid email, please recheck" });
+        }
+
+        const existinguser = await USER.findOne({ email });
+
         if (existinguser) {
-            return res.status(409).json({ errors: { email: "already an account with this email." } });
+            return res.status(409).json({ errors: { email: "Already an account with this email." } });
         }
 
         const newUser = await USER.create(req.body);
@@ -30,40 +32,3 @@ const createUser = async (req, res) => {
         res.status(500).json({ message: "Signup failed. Please try again." });
     }
 };
-
-const loginUser = async (req, res) => {
-    const { email, password } = req.body;
-
-    try {
-        const user = await USER.findOne({ email });
-
-        if (!user) {
-            return res.status(400).json({ message: "invallid email! Please try Signing in if you're new" });
-        }
-
-        const checkPassword = await encrypt.compare(password, user.password);
-
-        if (!checkPassword) {
-            return res.status(400).json({ message: "Incorrect password" });
-        }
-
-        const token = jwt.sign(
-            { id: user._id, email: user.email },
-            process.env.JWT_KEY,
-            { expiresIn: "1d" }
-        );
-
-        res.status(200).json({
-            user: {
-                id: user._id,
-                email: user.email,
-                username: user.username
-            },
-            token
-        });
-    } catch (error) {
-        res.status(500).json({ message: "Login failed. Please try again." });
-    }
-};
-
-module.exports = { createUser, loginUser };
